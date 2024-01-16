2e616bb2da6ad2fc0cf683b7bb6b113026bf8325
zo4564 <zosia46@gmail.com>
2024-01-16 18:38:21 +0100

    possible moves for jumps

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -28,8 +29,18 @@ def home():
         [-1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1],
         [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
     ]
+    board = randomizeBoard(board)
     return render_template('chinese_checkers.html', board=board)
 
+
+def randomizeBoard(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == -1:
+                continue
+            else:
+                board[i][j] = random.randint(0, 2)
+    return board
 
 if app.name == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
