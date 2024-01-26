be73ef249673d705e4e68552c06b8d79d241b449
zo4564 <zosia46@gmail.com>
2024-01-26 21:33:56 +0100

    prawie działa

--- app.py
+++ app.py
@@ -24,9 +24,16 @@ board = [
         [-1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1],
         [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
     ]
+
+possibleSteps = []
+possibleJumps = []
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 @app.route('/checkers')
 def game():
     global board
@@ -43,6 +50,7 @@ def randomizeBoard(board):
                 board[i][j] = random.randint(0, 2)
     return board
 
+
 @app.route('/update_board', methods=['POST'])
 def update_board():
     global board
@@ -56,16 +64,60 @@ def update_board():
     return jsonify(response)
 
 
+
 def makeMove(board, player, moveFrom, moveTo):
-    if board[moveFrom[0]][moveFrom[1]] == 1 and player == "player1":
-        board[moveFrom[0]][moveFrom[1]] = 0
-        board[moveTo[0][moveTo[1]]] = 1
-        print("player 1")
-    elif board[moveFrom[0]][moveFrom[1]] == 2 and player == "player2":
-        board[moveFrom[0]][moveFrom[1]] = 0
-        board[moveTo[0][moveTo[1]]] = 2
-        print("player 2")
+    toX = moveTo[1]
+    toY = moveTo[0]
+    fromX = moveFrom[1]
+    fromY = moveFrom[0]
+    if board[toX][toY] == 0:
+        possibleMoves= getPossibleMoves(board, fromX, fromY)
+        if moveTo in possibleMoves[0] or moveTo in possibleMoves[1]: #tu sie jebie
+            if player == "player1" and board[fromX][fromY] == 1:
+                board[fromX][fromY] = 0
+                board[toX][toY] = 1
+                print("player 1 moved")
+            elif player == "player2" and board[fromX][fromY] == 2:
+                board[fromX][fromY] = 0
+                board[toX][toY] = 2
+                print("player 2 moved")
     else:
         print("invalid move")
+        assert False
+
+def getPossibleMoves(board, x, y):
+
+    directions = [
+        (-1, 0), (0, -1), (0, 1), (1, 0)
+    ]
+    if y % 2 == 0:
+        directions.extend([(-1, -1), (-1, 1)])
+    else:
+        directions.extend([(1, -1), (1, 1)])
+
+    possible_steps = []
+    possible_jumps = []
+
+    for direction in directions:
+        searched_coords = (y - direction[0], x - direction[1])
+
+        if searched_coords[1] % 2 == 0:
+            searched_coords = (searched_coords[0] - 1, searched_coords[1])
+
+        # Check if searched coordinates are within the board bounds
+        if 0 <= searched_coords[1] < len(board) and 0 <= searched_coords[0] < len(board[0]):
+            if board[searched_coords[1]][searched_coords[0]] == 0:
+                possible_steps.append(searched_coords)
+            else:
+                next_coords = (y - (2 * direction[0]), x - (2 * direction[1]))
+
+                # Check if next coordinates are within the board bounds
+                if 0 <= next_coords[1] < len(board) and 0 <= next_coords[0] < len(board[0]):
+                    if board[next_coords[1]][next_coords[0]] == 0:
+                        possible_jumps.append(next_coords)
+
+    return possible_steps, possible_jumps
+
+
 if app.name == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
